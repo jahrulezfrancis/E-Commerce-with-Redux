30e367d551859fc2ad3ded3a0e65c5e1fc940b20
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-02-13 12:49:25 +0100

    Included Cart component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,17 +3,24 @@ import Header from "./Header";
 import Products from "./Products";
 import "./Layout.css";
 import CartItems from "./CartItems"
+import { useSelector } from "react-redux";
 
 
 const Layout = () => {
-  let total = 100;
+  let total = 0;
+  const ShowCart = useSelector((state) => state.Cart.ShowCart)
+  const ItemList =useSelector((state) => state.Cart.ItemList)
+
+  ItemList.forEach(item => {
+    total += item.totalPrice
+  });
 
   return (
     <React.Fragment>
       <div className="layout">
         <Header />
+        {ShowCart ? <CartItems /> : null}
         <Products />
-        <CartItems />
         <div className="total-price">
           <h3>Total: ${total}</h3>
           <button className="orderBtn">Place Order</button>
